ae6646cd5bcabf9ac2d09c402fb4f29a8bb7386e
rcernich <rcernich@redhat.com>
2011-07-15 07:35:24 -0600

    preliminary admin integration using as7 deployment processor

--- deploy/jboss-as7/extension/src/main/java/org/switchyard/as7/extension/deployment/SwitchYardDeploymentProcessor.java
+++ deploy/jboss-as7/extension/src/main/java/org/switchyard/as7/extension/deployment/SwitchYardDeploymentProcessor.java
@@ -18,9 +18,15 @@
  */
 package org.switchyard.as7.extension.deployment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.ee.component.EEModuleDescription;
 import org.jboss.as.naming.context.NamespaceContextSelector;
+import org.jboss.as.server.deployment.Attachments;
 import org.jboss.as.server.deployment.DeploymentPhaseContext;
 import org.jboss.as.server.deployment.DeploymentUnit;
 import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
@@ -28,14 +34,28 @@ import org.jboss.as.server.deployment.DeploymentUnitProcessor;
 import org.jboss.as.weld.WeldDeploymentMarker;
 import org.jboss.as.weld.services.BeanManagerService;
 import org.jboss.logging.Logger;
+import org.jboss.modules.Module;
 import org.jboss.msc.service.ServiceBuilder;
+import org.jboss.msc.service.ServiceController;
 import org.jboss.msc.service.ServiceController.Mode;
 import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceRegistry;
 import org.jboss.msc.service.ServiceTarget;
 import org.jboss.msc.value.ImmediateValue;
+import org.switchyard.admin.Component;
+import org.switchyard.admin.Service;
+import org.switchyard.admin.base.BaseApplication;
+import org.switchyard.admin.base.BaseService;
+import org.switchyard.admin.base.BaseSwitchYard;
 import org.switchyard.as7.extension.SwitchYardDeploymentMarker;
 import org.switchyard.as7.extension.SwitchYardExtension;
+import org.switchyard.as7.extension.admin.SwitchYardAdminService;
 import org.switchyard.as7.extension.services.SwitchYardService;
+import org.switchyard.config.model.composite.BindingModel;
+import org.switchyard.config.model.composite.CompositeModel;
+import org.switchyard.config.model.composite.CompositeServiceModel;
+import org.switchyard.config.model.composite.InterfaceModel;
+import org.switchyard.config.model.switchyard.SwitchYardModel;
 
 /**
  * Deployment processor that installs the SwitchYard service and all other dependent services.
@@ -75,16 +95,69 @@ public class SwitchYardDeploymentProcessor implements DeploymentUnitProcessor {
         switchyardServiceBuilder.setInitialMode(Mode.ACTIVE);
         switchyardServiceBuilder.install();
 
+        // we need the module's class loader in order to properly parse the
+        // configuration
+        final Module module = deploymentUnit.getAttachment(Attachments.MODULE);
+        ClassLoader origCL = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(module.getClassLoader());
+            registerContents(deploymentUnit, phaseContext.getServiceRegistry(), metaData);
+        } catch (Exception e) {
+            LOG.error("Error populating admin model.", e);
+        } finally {
+            Thread.currentThread().setContextClassLoader(origCL);
+        }
+
         processManagement(deploymentUnit, switchyardServiceName);
     }
 
     @Override
     public void undeploy(DeploymentUnit deploymentUnit) {
+        // TODO: unregister the services
     }
 
     private void processManagement(DeploymentUnit deploymentUnit, ServiceName switchyardServiceName) {
         deploymentUnit.createDeploymentSubModel(SwitchYardExtension.SUBSYSTEM_NAME,
                 PathElement.pathElement("service-name", switchyardServiceName.getCanonicalName()));
+    }
+
+    private void registerContents(DeploymentUnit deploymentUnit, ServiceRegistry serviceRegistry,
+            SwitchYardMetaData metaData) {
+        final ServiceController<?> controller = serviceRegistry.getRequiredService(SwitchYardAdminService.SERVICE_NAME);
+        BaseSwitchYard switchYard = BaseSwitchYard.class.cast(controller.getService().getValue());
+
+        SwitchYardModel switchYardModel = metaData.geSwitchYardModel();
+
+        QName applicationName = switchYardModel.getQName();
+        if (applicationName == null) {
+            applicationName = new QName(deploymentUnit.getName());
+        }
+        BaseApplication application = new BaseApplication(applicationName);
+
+        CompositeModel composite = switchYardModel.getComposite();
+        List<CompositeServiceModel> compositeServices = composite.getServices();
+        List<Service> services = new ArrayList<Service>(compositeServices.size());
+        for (CompositeServiceModel compositeService : compositeServices) {
+            final QName name = compositeService.getQName();
+            final String interfaceName = getServiceInterface(compositeService);
+            final Component implementation = switchYard.findComponent(compositeService.getComponent()
+                    .getImplementation().getType());
+            final List<Component> gateways = new ArrayList<Component>();
+            for (BindingModel binding : compositeService.getBindings()) {
+                gateways.add(switchYard.findComponent(binding.getType()));
+            }
+            services.add(new BaseService(name, interfaceName, application, implementation, gateways));
+        }
+        application.setServices(services);
+        switchYard.addApplication(application);
+    }
+    
+    private String getServiceInterface(CompositeServiceModel compositeService) {
+        InterfaceModel interfaceModel = compositeService.getInterface();
+        if (interfaceModel != null) {
+            return interfaceModel.getInterface();
+        }
+        return compositeService.getComponentService().getInterface().getInterface();
     }
 
 }
